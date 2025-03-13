48c2d0dd7cb4abda9f64b837d806106bcee4c00b
Muhammad Nouman <110379750+nomiwsd@users.noreply.github.com>
2025-03-13 22:35:36 +0500

    new changes

--- firebase.js
+++ firebase.js
@@ -2,14 +2,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
+// Load environment variables from .env file
+import dotenv from 'dotenv';
+dotenv.config();
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDX0nWW1KKhomHl-R9DGALrpFqQWPkVIdc",
-  authDomain: "voucher-code-the-ganges.firebaseapp.com",
-  projectId: "voucher-code-the-ganges",
-  storageBucket: "voucher-code-the-ganges.firebasestorage.app",
-  messagingSenderId: "8571493735",
-  appId: "1:8571493735:web:f21918a77353e0bb2823c2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
